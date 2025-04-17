406fa7738be6510ef63c9ef6497d7501467fe3b4
ricarth.lima <ricarth.lima@gmail.com>
2025-04-17 12:07:34 -0300

    usar tratamento

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,9 @@
 import 'package:dart_praticando_coleta/data/dados_corrompidos.dart';
 import 'package:dart_praticando_coleta/data/dados_integros.dart';
+import 'package:dart_praticando_coleta/exceptions/exceptions.dart';
 import 'package:dart_praticando_coleta/models/collection_point.dart';
 import 'package:dart_praticando_coleta/utils/processar_dados.dart';
+import 'package:dart_praticando_coleta/utils/validar_dados.dart';
 
 void main() {
   final listaDadosIntegros = dadosIntegros;
@@ -11,14 +13,22 @@ void main() {
 
   for (Map<String, dynamic> mapa in listaDadosCorrompidos) {
     try {
+      validarDados(mapa); // essa função chama a função de validação
       CollectionPoint collectionPoint = CollectionPoint.fromMap(mapa);
       listaParaProcessar.add(collectionPoint);
-    } on Error catch (e) {
-      print(e);
-    } on Exception catch (e) {
-      print(e);
+    } on InvalidIdException catch (e) {
+      print('❌ Erro de ID: ${e.toString()}');
+    } on EmptyFieldException catch (e) {
+      print('❌ Campo obrigatório ausente: ${e.toString()}');
+    } on EmptyMaterialsException catch (e) {
+      print('❌ Materiais inválidos: ${e.toString()}');
+    } on NegativeFillLevelException catch (e) {
+      print('❌ Nível de preenchimento inválido: ${e.toString()}');
+    } catch (e) {
+      print('❌ Erro inesperado ao processar o mapa: $e');
     }
   }
 
+  print("\n");
   processarDados(listaParaProcessar);
 }
